180c2a8772e9e599ab4c434400d9ed5555d35fee
imdarkcode <otavio.sudano13@gmail.com>
2026-09-02 20:30:43 -0300

    Automação do teste

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,11 +1,9 @@
 describe('Login no SauceDemo', () => {
     it('Deve realizar o login e apresentar os produtos', () => {
-        cy.visit('https://www.saucedemo.com')
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.url().should('include', '/inventory.html')
-        cy.get('[data-test="title"]').should('contain', 'Products')
-        cy.get('[data-test="inventory-list"]').should('be.visible')
+        cy.visit('https://example.cypress.io/commands/actions')
+        cy.get('.action-email').type('fake@email.com')
+        cy.get('.action-email').should('have.value', 'fake@email.com')
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').check()
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').should('be.checked')
     })
 })
